6a9d93fc3fe2ea50e8840473754422dda9ce6ec8
Javier Preciado <jpreciado85@gmail.com>
2024-09-02 11:14:36 -0500

    carga del conjunto de datos

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.datasets import fetch_california_housing
 
+# Cargar el conjunto de datos de viviendas en California
+cali_data = fetch_california_housing()
+df = pd.DataFrame(cali_data.data, columns=cali_data.feature_names)
+df['MedHouseVal'] = cali_data.target
+
+# Exploración de los datos
+print(df.head())
+print(df.info())
+print(df.describe())
+
+# Correlación entre características
+correlation = df.corr()
+print(correlation['MedHouseVal'].sort_values(ascending=False))
+
+# Visualización de la relación entre una característica y el valor medio de la vivienda
+plt.scatter(df['AveRooms'], df['MedHouseVal'])
+plt.xlabel('Número medio de habitaciones')
+plt.ylabel('Valor medio de la vivienda')
+plt.title('Relación entre número medio de habitaciones y valor medio de la vivienda')
+plt.show()
